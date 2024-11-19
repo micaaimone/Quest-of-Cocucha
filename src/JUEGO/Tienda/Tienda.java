d5005e7c731c04f80e2bb4c09d6ecd028e311df8
Micaela Aimone <mica.aimone@gmail.com>
2024-11-19 17:35:46 -0300

    agregamos metodos especificos de tienda

--- src/JUEGO/Tienda/Tienda.java
+++ src/JUEGO/Tienda/Tienda.java
@@ -3,8 +3,12 @@ package JUEGO.Tienda;
 import JUEGO.Exceptions.CorroborarException;
 import JUEGO.Personajes.Clases.Armas.Armas;
 import JUEGO.Personajes.Clases.Asesino;
+import JUEGO.Personajes.Clases.Guerrero;
+import JUEGO.Personajes.Clases.Mago;
 import JUEGO.Personajes.Personaje;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tienda {
@@ -15,13 +19,25 @@ public class Tienda {
 
     public void mostrarPociones(){
         for (Pocion p: Pocion.values()){
-            System.out.println(p);
+            System.out.println(" Nombre de la pocion: " + p +
+                                "Precio: " + p.getPrecio());
         }
     }
 
     public void mostrarArmas(){
         for (Armas a: Armas.values()){
-            System.out.println(a);
+            System.out.println("Nombre: " + a.name());
+            System.out.println("Precio: " + a.getPrecio());
+            System.out.println("Descripción: " + a.getDescripcion());
+            System.out.println("----------------------");
+        }
+    }
+
+    public void mostrarArmas(List<Armas> armas){
+        for (Armas a: Armas.values()){
+            System.out.println(" Nombre del arma: " + a +
+                    "\n Descripcion: " + a.getDescripcion() +
+                    "\n Precio: " + a.getPrecio());
         }
     }
 
@@ -73,12 +89,38 @@ public class Tienda {
         } else if (eleccion == 2){
             System.out.println("¿Que arma desea comprar?\n");
 
+            System.out.println("Tus opciones son: \n");
+            //para poder mostrar unicamente las armas disponibles de cada personaje armamos
+            // un metodo especifico que devuelva una lista de los mismos
+            List<Armas> listaArmas = new ArrayList<>();
             if (p instanceof Asesino){
-
+                listaArmas = obtenerArmasDelPersonaje(p);
+                System.out.println("Detalle de las armas disponibles para el Asesino\n");
+                mostrarArmas(listaArmas);
+            } else if (p instanceof Mago){
+                listaArmas = obtenerArmasDelPersonaje(p);
+                System.out.println("Detalle de las armas disponibles para el Mago\n");
+                mostrarArmas(listaArmas);
+            } else if (p instanceof Guerrero){
+                listaArmas = obtenerArmasDelPersonaje(p);
+                System.out.println("Detalle de las armas disponibles para el Guerrero\n");
+                mostrarArmas(listaArmas);
             }
 
         }
 
+    }
+
+    public List<Armas> obtenerArmasDelPersonaje(Personaje p){
+        List<Armas> listaArmas = new ArrayList<>();
+        String tipoPersonaje = p.getClass().getSimpleName();
+
+        for (Armas arma : Armas.values()) {
+            if (arma.getTipoPersonaje().equalsIgnoreCase(tipoPersonaje)) {
+                listaArmas.add(arma);
+            }
+        }
+        return listaArmas;
     }
 
 }
