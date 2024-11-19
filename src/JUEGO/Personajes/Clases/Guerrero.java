bee57b9ac0e19ed0c6abbf51e19f3096b36f3c7d
Micaela Aimone <mica.aimone@gmail.com>
2024-11-19 17:33:54 -0300

    acomodamos la clase para empezar a probar

--- src/JUEGO/Personajes/Clases/Guerrero.java
+++ src/JUEGO/Personajes/Clases/Guerrero.java
@@ -1,21 +1,22 @@
 package JUEGO.Personajes.Clases;
 
+import JUEGO.Exceptions.CorroborarException;
+import JUEGO.Personajes.Clases.Armas.Armas;
 import JUEGO.Personajes.Personaje;
 
 public class Guerrero extends Personaje {
+    public Guerrero() {
+        super(100, 80, 80, 0, 30, Armas.cuchillo);
+    }
 
 
+    @Override
+    public void atacar(Personaje enemy) throws CorroborarException {
 
+    }
 
+    @Override
+    public void curarse() throws CorroborarException {
 
-
-
-
-
-
-
-
-
-
-
+    }
 }
