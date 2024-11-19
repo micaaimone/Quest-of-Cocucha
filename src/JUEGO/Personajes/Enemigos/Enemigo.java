527846c3d22bfb111cdc997ac4538dcc95b71984
lautaro <lau9araya@gmail.com>
2024-11-18 22:19:17 -0300

    avance en nivel y puertas

--- src/JUEGO/Personajes/Enemigos/Enemigo.java
+++ src/JUEGO/Personajes/Enemigos/Enemigo.java
@@ -1,12 +1,13 @@
 package JUEGO.Personajes.Enemigos;
 
+import JUEGO.Personajes.Clases.Armas.Armas;
 import JUEGO.Personajes.Personaje;
 
 public class Enemigo extends Personaje {
 
 
     public Enemigo(int PH, int fuerza, int magia, int resistencia, int level) {
-        super(PH, fuerza, magia, resistencia, level);
+        super(PH, fuerza, magia, resistencia, level );
     }
 
     @Override
