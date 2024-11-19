931d08d530375792bf0166453a6f38d6b1da2311
Micaela Aimone <mica.aimone@gmail.com>
2024-11-18 22:29:06 -0300

    Cambios en curar atributo

--- src/JUEGO/Personajes/Movimientos.java
+++ src/JUEGO/Personajes/Movimientos.java
@@ -3,7 +3,7 @@ package JUEGO.Personajes;
 import JUEGO.Exceptions.CorroborarException;
 
 public interface Movimientos {
-    public void atacar(Personaje enemy);
+    public void atacar(Personaje enemy) throws CorroborarException;
     public void curarse() throws CorroborarException;
     public void subirNivel();
     public void muerte();
