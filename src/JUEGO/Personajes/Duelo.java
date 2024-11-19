5d47146d8e5c695a4756eb1aa5a2789d05301dd5
lautaro <lau9araya@gmail.com>
2024-11-19 01:00:37 -0300

    avance en la clase de duelo para combates

--- src/JUEGO/Personajes/Duelo.java
+++ src/JUEGO/Personajes/Duelo.java
@@ -1,32 +1,50 @@
 package JUEGO.Personajes;
 
+import JUEGO.Exceptions.CorroborarException;
 import JUEGO.Personajes.Enemigos.Enemigo;
 
 import java.util.Scanner;
 
 public class Duelo {
-    private int decision;
-    private int turnos;
+    private static int decision;
+    private static int turnos;
 
-    public void combate(Enemigo enemigo, Personaje personaje) {
+    public static void combate(Enemigo enemigo, Personaje personaje) {
         Scanner sc = new Scanner(System.in);
         while (enemigo.getPH()<= 0 || personaje.getPH()<= 0) {
             System.out.println("Cual es tu proximo movimiento?");
             System.out.println("1. Atacar \n 2. Curarse");
 
-            decision = sc.nextInt();
+            try {
+                decision = sc.nextInt();
+                sc.nextLine();
+            }catch (NumberFormatException e) {
+                System.out.println("Error: ingrese solo numeros");
+            }
+
 
             switch (decision){
+
                 case 1:
-                    personaje.atacar(enemigo);
+                    try {
+                        personaje.atacar(enemigo);
+                    }catch (CorroborarException e){
+                        System.out.println(e.getMessage());
+                    }
                     break;
 
                     case 2:
-                        personaje.curarse();
+                        try {
+                            personaje.curarse();
+                        }catch (CorroborarException e){
+                            System.out.println(e.getMessage());
+                        }
                     break;
                     default:
                         break;
             }
+
+            enemigo.atacar(personaje);
             turnos++;
         }
     }
