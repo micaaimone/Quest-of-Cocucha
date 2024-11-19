ed7ee6c3172c6d8a456189a724fc7c7a176f07f4
lautaro <lau9araya@gmail.com>
2024-11-18 22:59:10 -0300

    avance en nivel y puertas y clase de duelo

--- src/JUEGO/Nivel/GestionNivel.java
+++ src/JUEGO/Nivel/GestionNivel.java
@@ -1,12 +1,41 @@
 package JUEGO.Nivel;
 import JUEGO.Personajes.*;
+import JUEGO.Personajes.Enemigos.Enemigo;
 
 public class GestionNivel {
 
     public void crearNivel (){
         //----------------ENEMIGOS------------------------------------
+        Enemigo enemigo1 = new Enemigo();
+        Enemigo enemigo2 = new Enemigo();
+        Enemigo enemigo3 = new Enemigo();
+        Enemigo enemigo4 = new Enemigo();
+        Enemigo enemigo5 = new Enemigo();
+        //-------tienda--------------------------------------------
 
-        //----------------------------------------------------
+        //-------Puertas--------------------------------------------
+        PuertaEnemigo puertaEnemigo1 = new PuertaEnemigo(enemigo1);
+        PuertaEnemigo puertaEnemigo2 = new PuertaEnemigo(enemigo2);
+        PuertaEnemigo puertaEnemigo3 = new PuertaEnemigo(enemigo3);
+        PuertaEnemigo puertaEnemigo4 = new PuertaEnemigo(enemigo4);
+        PuertaEnemigo puertaEnemigo5 = new PuertaEnemigo(enemigo5);
+        //PUERTAS TIENDA
 
+
+        //-------niveles--------------------------------------------
+        Nivel nivel1 = new Nivel(1);
+        nivel1.agregarPuerta(puertaEnemigo1);
+        //--
+        Nivel nivel2 = new Nivel(2);
+        nivel2.agregarPuerta(puertaEnemigo2);
+        //--
+        Nivel nivel3 = new Nivel(3);
+        nivel3.agregarPuerta(puertaEnemigo3);
+        //--
+        Nivel nivel4 = new Nivel(4);
+        nivel4.agregarPuerta(puertaEnemigo4);
+        //--
+        Nivel nivel5 = new Nivel(5);
+        nivel5.agregarPuerta(puertaEnemigo5);
     }
 }
