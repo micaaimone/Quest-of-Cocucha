6b75e3104201a0e3271e8c96b71858c391010d90
Micaela Aimone <mica.aimone@gmail.com>
2024-11-19 01:37:11 -0300

    agregue cambios en armas + cosas a cambiar

--- src/JUEGO/Personajes/Clases/Armas/Armas.java
+++ src/JUEGO/Personajes/Clases/Armas/Armas.java
@@ -2,27 +2,39 @@ package JUEGO.Personajes.Clases.Armas;
 
 public enum Armas {
 
+    //agregar descripciones para armas, diria q agregar + armas para cada personaje
+    espadaCorta (100),
+    espadaGrande(100),
+    bastonCorto(100),
+    bastonLargo(100),
+    ballesta(100),
+    cuchillo(100);
 
-    espadaCorta,
-    espadaGrande,
-    bastonCorto,
-    bastonLargo,
-    ballesta,
-    cuchillo;
-
-
-
-
+    private final int precio;
 
 
+    Armas(int precio) {
+        this.precio = precio;
+    }
 
-
-
-
-
-
-
-
-
-
+    public int getPrecio() {
+        return precio;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
