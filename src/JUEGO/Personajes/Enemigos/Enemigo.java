2a8da4f29765247fc0cef55e15899f875e1505c6
lautaro <lau9araya@gmail.com>
2024-11-18 22:59:10 -0300

    avance en nivel y puertas y clase de duelo
    # Conflicts:
    #	src/JUEGO/Personajes/Enemigos/Enemigo.java

--- src/JUEGO/Personajes/Enemigos/Enemigo.java
+++ src/JUEGO/Personajes/Enemigos/Enemigo.java
@@ -11,12 +11,6 @@ public class Enemigo extends Personaje {
         super(PH, fuerza, magia, resistencia, level , Armas.cuchillo );
     }
 
-    public Enemigo() {
-    }
-
-    public Enemigo() {
-    }
-
     @Override
     public void atacar(Personaje enemy) {
 
