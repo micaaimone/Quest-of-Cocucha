4c7454b45a9da8e3ec36a3e146b4c3d255dcec13
Micaela Aimone <mica.aimone@gmail.com>
2024-11-20 11:32:01 -0300

    se agrego el nombre como atributo

--- src/JUEGO/Armas/Armas.java
+++ src/JUEGO/Armas/Armas.java
@@ -3,19 +3,21 @@ package JUEGO.Armas;
 public enum Armas {
 
     //mejorar las descripciones
-    espadaCorta(0, "espada normal de guerrero", "Guerrero"),
-    espadaGrande(100, "espada guerrero pro - al comprar la mejora aumenta la resistencia maxima 10 puntos(sube esos puntos automaticamente tmbn en resistencia) y aumenta el poder de ataque por 15 pts ", "Guerrero"),
-    bastonCorto(0, "bastoncito mago", "Mago"),
-    bastonLargo(100, "baston mago pro - al comprar la mejora aumenta la magia maxima 10 puntos(sube esos puntos automaticamente tmbn en magia) y aumenta el poder de ataque por 15 pts ", "Mago"),
-    cuchillo(0, "cuchillo asesino", "Asesino"),
-    dagaFilosa(100, "daga filosa  asesino - al comprar la mejora aumenta la resistencia maxima 10 puntos(sube esos puntos automaticamente tmbn en resistencia) y aumenta el poder de ataque por 15 pts ", "Asesino");
+    espadaCorta("Espada corta",0, "espada normal de guerrero", "Guerrero"),
+    espadaGrande("Espada grande",100, "espada guerrero pro - al comprar la mejora aumenta la resistencia maxima 10 puntos(sube esos puntos automaticamente tmbn en resistencia) y aumenta el poder de ataque por 15 pts ", "Guerrero"),
+    bastonCorto("Baston corto",0, "bastoncito mago", "Mago"),
+    bastonLargo("Baston largo",100, "baston mago pro - al comprar la mejora aumenta la magia maxima 10 puntos(sube esos puntos automaticamente tmbn en magia) y aumenta el poder de ataque por 15 pts ", "Mago"),
+    cuchillo("Cuchillo",0, "cuchillo asesino", "Asesino"),
+    dagaFilosa("Daga filosa",100, "daga filosa  asesino - al comprar la mejora aumenta la resistencia maxima 10 puntos(sube esos puntos automaticamente tmbn en resistencia) y aumenta el poder de ataque por 15 pts ", "Asesino");
 
 
     private final int precio;
     private final String descripcion;
     private final String tipoPersonaje;
+    private final String nombre;
 
-    Armas(int precio, String descripcion, String tipoPersonaje) {
+    Armas(String nombre, int precio, String descripcion, String tipoPersonaje) {
+        this.nombre = nombre;
         this.precio = precio;
         this.descripcion = descripcion;
         this.tipoPersonaje = tipoPersonaje;
@@ -32,6 +34,10 @@ public enum Armas {
 
     public String getTipoPersonaje() {
         return tipoPersonaje;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 }
 
