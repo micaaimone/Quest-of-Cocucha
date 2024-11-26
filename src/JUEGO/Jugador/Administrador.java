395c5a4fb44c465fdab326a334c469cc8bc51ea3
lautaro <lau9araya@gmail.com>
2024-11-26 18:37:03 -0300

    continuamiento

--- src/JUEGO/Jugador/Administrador.java
+++ src/JUEGO/Jugador/Administrador.java
@@ -25,7 +25,28 @@ public class Administrador {
         return password;
     }
 
+    public boolean validarPassword(String password) {
 
+        return false;
+    }
+
+    public void administrar(){
+        System.out.println("Bienvenido a zona administradora");
+        Boolean validar = false;
+
+        while(!validar) {
+            System.out.println("comprobemos que eres un administrador! \n Ingrese la contraseña: ");
+            Scanner sc = new Scanner(System.in);
+            String password = sc.nextLine();
+            validar = validarPassword(password);
+            if(!validar) {
+                System.out.println("Contraseña incorrecta, volve a intentarlo");
+            }
+        }
+        System.out.println("Bienvenido admin!");
+
+
+    }
 
 
 }
