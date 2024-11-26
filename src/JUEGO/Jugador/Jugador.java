906af1bbcb4be7f5845edaaeda566fd25e0a03a5
lautaro <lau9araya@gmail.com>
2024-11-26 00:11:23 -0300

    eliminacion de metodos innutilizados y correcion

--- src/JUEGO/Jugador/Jugador.java
+++ src/JUEGO/Jugador/Jugador.java
@@ -3,6 +3,7 @@ package JUEGO.Jugador;
 import JUEGO.Personajes.Clases.Asesino;
 import JUEGO.Personajes.Clases.Guerrero;
 import JUEGO.Personajes.Clases.Mago;
+import JUEGO.Personajes.CrearPersonaje;
 import JUEGO.Personajes.Personaje;
 
 import java.util.Scanner;
@@ -19,7 +20,7 @@ public class Jugador {
 
     public Jugador(String nombre) {
         Nombre = nombre;
-        this.personaje = ElegirPersonaje();
+        this.personaje = CrearPersonaje.Seleccionar();
         Puntuacion = 0;
         this.id = idGlobal ++;
     }
@@ -58,54 +59,6 @@ public class Jugador {
     public int getIdGlobal() {
         return idGlobal;
     }
-
-    private String IngresarNombre(String Nombre) {
-
-        this.Nombre = this.Nombre = Nombre;
-
-        return "el nombre se ah creado correctamente";
-
-    }
-
-
-    public Personaje ElegirPersonaje() {
-
-        Personaje p = null;
-        int opcion;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nSeleccione un personaje, elija el nro\n");
-        System.out.println("\n1-Guerrero\n");
-        //aca va la desc del guerrero
-        System.out.println("\n2-Mago\n");
-        //aca va la desc del mago
-        System.out.println("\n3-Asesino\n");
-        //aca va la desc del asesino
-        opcion = scanner.nextInt();
-        scanner.nextLine();
-
-        switch (opcion) {
-            case 1:
-                Guerrero guerrero = new Guerrero();
-                p = guerrero;
-                break;
-            case 2:
-                Mago mago = new Mago();
-                p = mago;
-                break;
-            case 3:
-                Asesino asesino = new Asesino();
-                p = asesino;
-                break;
-            default:
-                System.out.println("No ingreso una opcion correcta");
-                break;
-        }
-
-        return p;
-
-    }
-
-
 
 
     @Override
