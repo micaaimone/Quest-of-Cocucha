6b7279244254e2d5d47bed7c2d06145fd253b260
Micaela Aimone <mica.aimone@gmail.com>
2024-11-26 16:38:16 -0300

    ponemos un try catch para q solo se admitan nros

--- src/JUEGO/PantallaPrincipal.java
+++ src/JUEGO/PantallaPrincipal.java
@@ -9,6 +9,7 @@ import JUEGO.Personajes.CrearPersonaje;
 import JUEGO.Personajes.Personaje;
 import JUEGO.Tienda.Tienda;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //DSPS borrar corroborar excepcion
@@ -49,46 +50,55 @@ public class PantallaPrincipal {
             System.out.println("1-Jugar\n");
             System.out.println("2-Puntuacion\n");
             System.out.println("3-Salir\n");
-            opcion = scanner.nextInt();
-            scanner.nextLine();
-            switch (opcion) {
-                case 1:
-                    // llamamos a funcion para crear el jugador
 
-                    //hacer try catch hasta new jugador, sino salta error si se ingresa un nro incorrecto o letra
-                    System.out.println("Ingrese su nombre\n");
-                    String nombreJugador = scanner.nextLine();
-                    Jugador jugador = new Jugador(nombreJugador);
-                    ControlPantalla.limpiarPantalla();
-                    System.out.println("Bienvenido " + nombreJugador);
-                    System.out.println("Haz elegido el personaje: ");
+            try {
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+                switch (opcion) {
+                    case 1:
+                        // llamamos a funcion para crear el jugador
 
-                    // llevamos el jugador a recorrer el juego
+                        //hacer try catch hasta new jugador, sino salta error si se ingresa un nro incorrecto o letra
+                        System.out.println("Ingrese su nombre\n");
+                        String nombreJugador = scanner.nextLine();
+                        Jugador jugador = new Jugador(nombreJugador);
+                        ControlPantalla.limpiarPantalla();
+                        System.out.println("Bienvenido " + nombreJugador);
+                        System.out.println("Haz elegido el personaje: " + jugador.getPersonaje().getClass().getSimpleName());
 
-                    GestionNivel juego = new GestionNivel(jugador.getPersonaje());
-                    try {
-                        juego.Trayecto();
-                    } catch (CorroborarException e) {
-                        System.out.println(e.getMessage());
+                        // llevamos el jugador a recorrer el juego
 
-                    }
-                    jugador.setPuntuacion(juego.getPuntuacion());
+                        GestionNivel juego = new GestionNivel(jugador.getPersonaje());
+                        try {
+                            juego.Trayecto();
+                        } catch (CorroborarException e) {
+                            System.out.println(e.getMessage());
 
-                    jugadores.agregarJugador(jugador);
+                        }
+                        jugador.setPuntuacion(juego.getPuntuacion());
 
-                    break;
-                case 2:
-                    //llevaria a mostrar punt
-                    System.out.println("estas son las 5 puntuaciones mas altas");
-                    break;
-                case 3:
-                    //sale
-                    System.out.println("Salir");
-                    break;
-                default:
-                    System.out.println("No ingresaste una opcion correcta \n");
-                    break;
+                        jugadores.agregarJugador(jugador);
+
+                        break;
+                    case 2:
+                        //llevaria a mostrar punt
+                        System.out.println("estas son las 5 puntuaciones mas altas");
+                        break;
+                    case 3:
+                        //sale
+                        System.out.println("Salir");
+                        break;
+                    default:
+                        System.out.println("No ingresaste una opcion correcta \n");
+                        break;
+                }
+            } catch (InputMismatchException ex){
+                scanner.nextLine();
+                System.out.println("Solo se pueden ingresar numeros(1,2,3). Vuelve a intentarlo");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
+
         }
 
     }
